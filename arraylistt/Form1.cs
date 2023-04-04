283f59514cb52a4e10a70f69b48c3bd9c34e363d
55459 <55459@DESKTOP-8POE8QT>
2023-04-03 21:33:32 -0300

    Adicionar arquivos de projeto.

---
+++ arraylistt/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace arraylistt
+{
+    public partial class Form1 : Form
+    {
+        ArrayList tarefa = new ArrayList();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (tarefa.Contains(textBox1.Text)) // método contains = verifica se o valor existe na lista
+            {
+                MessageBox.Show("A tarefa já existe na lista");
+            }
+            else
+            {
+                
+                MessageBox.Show("Tarefa cadastrada!");
+                tarefa.Add(textBox1.Text);
+                textBox1.Clear();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (var x in tarefa) //forech se usa quando nao se sabe a quantidade de repetições
+                                    //var x vai guardar os dados do array func
+            {
+                tarefas.Items.Add(x);
+                // pra adicionar na list box 
+
+            }
+            
+            qntdtarefas.Items.Add(tarefa.Count);
+
+
+        }
+    }
+}
